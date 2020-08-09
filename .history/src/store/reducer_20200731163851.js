b85d009e279065862d6cfe4c64142f87a610b350
王英 <wangying@wangyingdeMBP.lan>
2020-08-09 21:54:01 +0800

    clear

--- .history/src/store/reducer_20200731163851.js
+++
@@ -1,8 +0,0 @@
-//combineReducers将小的reducer合并
-import { combineReducers } from 'redux'
-import { Home } from '../components/Home/store/reducer'
-
-const reducer = combineReducers({
-    Home
-});
-export default reducer;
